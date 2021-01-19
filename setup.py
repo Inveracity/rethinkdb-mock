493c6925030d74e4969fcc252bda50f0834ae588
Christopher Baklid <christopherb@unity3d.com>
2021-01-19 13:34:02 +0100

    add classifiers and python version requirement

--- setup.py
+++ setup.py
@@ -14,5 +14,11 @@ setup(
     maintainer_email="cbaklid@gmail.com",
     packages=['rethinkdb_mock'],
     package_dir={'rethinkdb_mock': 'rethinkdb_mock'},
-    install_requires=['rethinkdb>=2.4.8', 'python-dateutil', 'future']
+    install_requires=['rethinkdb>=2.4.8', 'python-dateutil', 'future'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
 )
