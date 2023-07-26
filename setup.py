3cb15d821a44ab192cb1086e4d454ce6f8c27e7d
Inveracity <cbaklid@gmail.com>
2023-07-26 10:10:33 +0200

    set version to 0.11.0-dev

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name="rethinkdb_mock",
     zip_safe=True,
-    version='0.10.1',
+    version='0.11.0-dev',
     description="A pure-python in-memory mock of rethinkdb (formerly MockThink)",
     url="https://github.com/Inveracity/rethinkdb-mock",
     maintainer="Christopher Baklid",
